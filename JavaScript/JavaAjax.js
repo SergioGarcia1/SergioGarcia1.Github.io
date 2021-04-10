35bcbeadac69643a98519fc614f37a9582aa3eac
SergioGarcia1 <80007667+SergioGarcia1@users.noreply.github.com>
2021-04-10 00:29:35 -0500

    Add files via upload

--- JavaScript/JavaAjax.js
+++ JavaScript/JavaAjax.js
@@ -109,9 +109,4 @@ document.querySelector("#btnconsulta").addEventListener("click",function(){
     
 });
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 43.5293101, lng: -5.6773233},
-      zoom: 13
-    });
-  }
+
